a88afc3c6279b6a1b95a0220030f57a4cc4c333f
Baptiste C.L. Mary <bclmary@mailoo.org>
2017-11-23 13:04:36 +0100

    update tree organisation to fit standard

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 # -*-coding:utf-8 -*
 
-from setuptools import setup
+from setuptools import setup, find_packages
+
+MAJOR = 0
+MINOR = 1
+PATCH = 1
+# VERSION = "{}.{}.{}".format(MAJOR, MINOR, PATCH)
+VERSION = f"{MAJOR}.{MINOR}.{PATCH}"
+
+# Set version
+with open("eccw/version.py", "w") as f:
+    f.write("__version__ = '{}'\n".format(VERSION))
 
 # Get version
 with open('eccw/version.py') as f:
@@ -30,16 +40,21 @@ setup(
     # authors= setted above
     author_email='bclmary@mailoo.org',
     license='MIT',
-    packages=['eccw'],
+    packages=find_packages(exclude=['doc', 'test*']),
     install_requires=[
         'all',
         'needed',
         'third',
-        'party'
+        'party',
+        'numpy>=1.0,<1.13'
         ],
-    scripts=['bin/eccw'],
-    zip_safe=False,
     include_package_data=True,
+    # scripts=['bin/eccw'],
+    entry_points={'console_scripts': [
+        'eccw=bin.eccw:main',
+        ],
+    },
+    zip_safe=False,
     # test_suite='nose.collector',
     # tests_require=['nose'],
     )
