f27e120a1134862bec69c79fcd062fbddfa49205
Baptiste C.L. Mary <bclmary@mailoo.org>
2017-11-28 12:00:32 +0100

    modify setup.py

--- setup.py
+++ setup.py
@@ -3,31 +3,47 @@
 
 from setuptools import setup, find_packages
 
-MAJOR = 0
-MINOR = 1
-PATCH = 1
-# VERSION = "{}.{}.{}".format(MAJOR, MINOR, PATCH)
-VERSION = f"{MAJOR}.{MINOR}.{PATCH}"
+MAJOR = 2
+MINOR = 0
+PATCH = 0
+VERSION = "{}.{}.{}".format(MAJOR, MINOR, PATCH)
 
-# Set version
+AUTHORS = (
+    "BCL Mary",
+    "Xiaoping Yuan",
+    "YM Leroy",
+    "Pauline Souloumiac",
+    "Chong Wu",
+    )
+
+# Set version.py
 with open("eccw/version.py", "w") as f:
     f.write("__version__ = '{}'\n".format(VERSION))
 
-# Get version
-with open('eccw/version.py') as f:
-    exec(f.read())
-    f.close()
-# Get authors
-with open('eccw/authors.py') as f:
-    exec(f.read())
-    f.close()
+# Set authors.py
+with open("eccw/authors.py", "w") as f:
+    pad = " " * 4
+    text = "__authors_list__ = [\n" + pad + "{}\n" + pad + "]\n"
+    joiner = ",\n" + pad
+    f.write(text.format(joiner.join(AUTHORS)))
+    f.write("__authors__ = ', '.join(__authors_list__)\n")
+
+# # Get version
+# with open('eccw/version.py') as f:
+#     exec(f.read())
+#     f.close()
+# # Get authors
+# with open('eccw/authors.py') as f:
+#     exec(f.read())
+#     f.close()
 
 setup(
     name='eccw',
-    # version= setted above
+    version=VERSION,
     description='Exact Critical Coulomb Wedge',
     long_description='Tools to compute and display the exact solution of any'
                      'parameter of Critical Coulomb Wedge',
+    keywords=['Geology', 'accretionnary prism'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: X11 Applications :: Qt',
@@ -37,7 +53,7 @@ setup(
         'Topic :: Scientific/Engineering ::  Geology',
         ],
     url='http://github.com/???',
-    # authors= setted above
+    authors=AUTHORS,
     author_email='bclmary@mailoo.org',
     license='MIT',
     packages=find_packages(exclude=['doc', 'test*']),
